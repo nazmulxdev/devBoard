7b3eef97366613eb5f0d3334ba3c09bd665f6a4d
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-03-01 14:13:12 +0600

    controlled all the complete button and added alert when the task is done as well as alert when all the task are complete

--- scripts/home.js
+++ scripts/home.js
@@ -8,22 +8,53 @@ document.getElementById("new-today").addEventListener("click", function () {
 
 
 // body section color change
-const uniqueBgColor = ["#cdb4db", "#ffc8dd", "#faedcd", "#be95c4","#c6ac8f","#f27059","#a4c3b2","#a3b18a","#dad7cd","#F1F5F9"];
+const uniqueBgColor = ["#cdb4db", "#ffc8dd", "#faedcd", "#be95c4", "#c6ac8f", "#f27059", "#a4c3b2", "#a3b18a", "#dad7cd", "#F1F5F9"];
 let i = 0;
 document.getElementById("theme-btn").addEventListener("click", function () {
     document.body.style.backgroundColor = uniqueBgColor[i];
     i++;
-    if (i >= uniqueBgColor.length){
+    if (i >= uniqueBgColor.length) {
         i = 0;
-    }       
+    }
 }
 );
 
 
 // date Task
-const realDate=new Date();
-const realTimeDate=realDate.toDateString().split(" ");
-const div=document.createElement("div");
-div.innerHTML=`<p class="font-medium text-lg">${realTimeDate[0]},</p><h1 class=font-bold text-lg>${realTimeDate[1]} ${realTimeDate[2]}  ${realTimeDate[3]}</h1>`;
+const realDate = new Date();
+const realTimeDate = realDate.toDateString().split(" ");
+const div = document.createElement("div");
+div.innerHTML = `<p class="font-medium text-lg">${realTimeDate[0]},</p><h1 class=font-bold text-lg>${realTimeDate[1]} ${realTimeDate[2]}  ${realTimeDate[3]}</h1>`;
 document.getElementById("realTime-Date").appendChild(div);
 
+
+
+
+
+// complete button task
+
+
+const completeBtn = document.querySelectorAll(".complete-btn");
+
+let totalTask = document.getElementById("task-remain").innerText;
+
+let convertedTotalTask = parseInt(totalTask);
+let totalCompleteTask = document.getElementById("task-complete").innerText;
+let convertedTotalCompleteTask = parseInt(totalCompleteTask);
+for (let btn of completeBtn) {
+    btn.addEventListener("click", function (event) {
+        convertedTotalTask--;
+        convertedTotalCompleteTask++;
+        if(convertedTotalTask<=completeBtn.length){
+            alert("Board Update Successfully");
+        }
+
+        if(convertedTotalTask<=0){alert("Congrats!!!You have complete all the current task.")}
+            
+        let finalTotalTask = Math.max(0,convertedTotalTask);
+        document.getElementById("task-remain").innerText=finalTotalTask;
+        document.getElementById("task-complete").innerText=convertedTotalCompleteTask;
+        event.target.disabled=true;
+    })
+
+}
